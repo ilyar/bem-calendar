179d52962e6056af429a56a72c3c5ae9254450b7
Ilya Rogov <ilyar.software@gmail.com>
2014-05-12 17:19:27 +0400

    update config project

--- .bem/make.js
+++ .bem/make.js
@@ -1,28 +1,20 @@
-/* jshint node:true */
-/* global MAKE */
+/* global MAKE:false */
 
-var environ = require('bem-environ')(__dirname);
-environ.extendMake(MAKE);
+// process.env.YENV = 'production';
 
-//process.env.YENV = 'production';
-//process.env.XJST_ASYNCIFY = 'yes';
+var PATH = require('path');
 
 MAKE.decl('Arch', {
 
-    blocksLevelsRegexp: /^.+?\.blocks/,
-    bundlesLevelsRegexp: /^.+?\.bundles$/,
-
-    libraries: [
-        'bem-core @ f4b46ef0590549042d938f7e981df4d14eb4caef',
-        'bem-components @ 82301a8af6c15c2849d1f755a24f594de6522251'
-    ]
+    blocksLevelsRegexp : /^.+?\.blocks/,
+    bundlesLevelsRegexp : /^.+?\.bundles$/
 
 });
 
 
 MAKE.decl('BundleNode', {
 
-    getTechs: function() {
+    getTechs : function() {
 
         return [
             'bemjson.js',
@@ -31,16 +23,38 @@ MAKE.decl('BundleNode', {
             'bemhtml',
             'browser.js+bemhtml',
             'css',
-            'ie.css',
             'html'
         ];
 
     },
 
-    'create-browser.js+bemhtml-optimizer-node': function(tech, sourceNode, bundleNode) {
-        sourceNode.getFiles().forEach(function(f) {
-            this['create-js-optimizer-node'](tech, this.ctx.arch.getNode(f), bundleNode);
-        }, this);
+    getForkedTechs : function() {
+        return this.__base().concat(['browser.js+bemhtml']);
+    },
+
+    getLevelsMap : function() {
+        return {
+            desktop : [
+                'libs/bem-core/common.blocks',
+                'libs/bem-core/desktop.blocks',
+                'libs/bem-components/common.blocks',
+                'libs/bem-components/desktop.blocks',
+                'libs/bem-components/design/common.blocks',
+                'libs/bem-components/design/desktop.blocks',
+                'common.blocks',
+                'desktop.blocks'
+            ]
+        };
+    },
+
+     getLevels : function() {
+        var resolve = PATH.resolve.bind(PATH, this.root),
+            buildLevel = this.getLevelPath().split('.')[0],
+            levels = this.getLevelsMap()[buildLevel] || [];
+
+        return levels
+            .map(function(path) { return resolve(path); })
+            .concat(resolve(PATH.dirname(this.getNodePrefix()), 'blocks'));
     }
 
 });
